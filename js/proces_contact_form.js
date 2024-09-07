f3992121fb933c1f8d1c8469cf69a098af213faa
Thinh Dinh <thinhdinh@ieee.org>
2024-09-07 23:03:27 +0800

    success page

--- js/proces_contact_form.js
+++
@@ -1,5 +0,0 @@
-/*make js to process the contact form*/
-
-document.querySelector('form').addEventListener('submit', function() {
-    document.querySelector('input[name="success"]').value = 'true';
-  });
